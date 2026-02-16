b568075a87d2c91ad0ea7743bb82058f86cde571
Hariram-Lab <ecisiisc@gmail.com>
2026-02-16 14:38:43 +0530

    design testbenches constraintgs added

---
+++ DESIGN/uart_rtl.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/16/2026 02:33:25 PM
+// Design Name: 
+// Module Name: uart_rtl
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module uart_rtl(
+
+    );
+endmodule
