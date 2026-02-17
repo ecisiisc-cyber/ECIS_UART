d83b6fe0f7c2bfb3be7d5e4ce53e51d69fb86bc2
Hariram-Lab <ecisiisc@gmail.com>
2026-02-17 12:47:20 +0530

    hierarchial design files added

---
+++ DESIGN/Baudrate_gen.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/17/2026 12:46:32 PM
+// Design Name: 
+// Module Name: Baudrate_gen
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Baudrate_gen(
+
+    );
+endmodule
---
+++ DESIGN/TX.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/17/2026 12:46:01 PM
+// Design Name: 
+// Module Name: TX
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module TX(
+
+    );
+endmodule
